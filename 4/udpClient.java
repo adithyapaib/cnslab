6ce7d4168b9965b57bcf0571c60b98a45be3efce
Adithya Pai B <paiadithya26@gmail.com>
2022-03-23 11:54:43 +0530

    UDP Client-Server Update

--- 4/udpClient.java
+++ 4/udpClient.java
@@ -2,11 +2,9 @@
 import java.net.*;
 public class udpClient {
     public static void main(String args [])throws Exception{
-       DatagramSocket ds = new DatagramSocket(4000);
          DatagramPacket dp = new DatagramPacket(new byte[100],100);
-            ds.receive(dp);
+            new DatagramSocket(4000).receive(dp);
             System.out.println("Message Received");
-            System.out.println(new String(dp.getData()));
-            ds.close();     
+            System.out.println(new String(dp.getData())); 
     }
 }
