de36264f8a710eb66f9df99e3b60f5b99baf20d6
Adithya Pai B <paiadithya26@gmail.com>
2022-03-23 11:20:36 +0530

    LeakyBucket : minified code

--- 6/leakyBucket.java
+++ 6/leakyBucket.java
@@ -5,41 +5,33 @@ class Main {
     static int bktSize , pkt , nofpkt , i , time =0 , oprate;
     public  static void main(String args[]) throws Exception {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter the BucketSize");
+        System.out.print("Enter the BucketSize \t");
         bktSize = s.nextInt();
-        System.out.println("Enter the no Packets");
+        System.out.print("\nEnter the no Packets \t");
         nofpkt = s.nextInt();
-        System.out.println("Enter the output rate");
+        System.out.print("\nEnter the output rate \t");
         oprate = s.nextInt();
         Random r = new Random();
         for (i=0;i<nofpkt;i++){
             pkt = r.nextInt(100);
-            System.out.println("******************************");
-            System.out.println("Packet "+i+ " of size " + pkt);
-            System.out.println("******************************");
+            System.out.println("\n\n+****************************+");
+            System.out.println("| Packet "+i+ " of size        " + pkt + " |");
+            System.out.println("+****************************+");
             time = leakyBucket(bktSize,oprate,pkt,time);
 
         }
         
     }
     public static int leakyBucket(int bktSize,int oprate,int pkt,int time){
-       if (bktSize < pkt){
-           System.out.println("Time "+ time + "s \nPacket dropped");
-           return time;
-       }
+       if (bktSize < pkt)
+           System.out.println("Time "+ (time+=1) + "s \nPacket wont fit in bucket ! \nPacket dropped");  
        else{
-           while (pkt> oprate){
-                time++;
-                pkt = pkt - oprate;
-                System.out.println("\nTime" + time +"s \nSent Packet Size of" + oprate + "\n Remaining Packet Size " + pkt);
-           }
-           if (pkt >0){
-               time++;
-               System.out.println("\nTime" + time+"s\nlast Packet Size " + pkt);
-                pkt = 0;
-           }
-              return time;
+           while (pkt> oprate)
+             System.out.println("Time :" + (time+=1) +" s \nSent Packet Size of " + oprate + "\nRemaining Packet Size " + (pkt -=oprate) + "\n");
+           if (pkt >0)
+               System.out.println("Time" +(time+=1)+"s\nLast Packet Size " + pkt);
        }
+       return time;
     }
 
 }
