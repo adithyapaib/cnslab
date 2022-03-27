c953ef57ebb0e8143dd52b8304a73ce8ab10ef26
Adithya Pai B <paiadithya26@gmail.com>
2022-03-27 17:03:01 +0530

    TCP client minified

--- 3/TCPclient.java
+++ 3/TCPclient.java
@@ -1,18 +1,21 @@
+
 import java.net.*;
-import java.util.Scanner;
+import java.util.*;
 import java.io.*;
 
-public class TCPclient {
+public class tc {
     public static void main(String args[]) throws Exception {
-        Socket sock = new Socket("127.0.0.1", 4000);
-        Scanner s = new Scanner(System.in);
+        
         System.out.print("Enter the file name :\t");
-        String fname =  s.nextLine();
-        PrintWriter pwrite = new PrintWriter(sock.getOutputStream(), true);
-        pwrite.println(fname);
+        String fname = new Scanner(System.in).nextLine() , str;
+
+
+        Socket sock = new Socket("127.0.0.1", 4000);
+        new PrintWriter(sock.getOutputStream(), true).println(fname);
+
+
         System.out.println("Contents of the File:");
         BufferedReader fileRead = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-        String str;
         while ((str = fileRead.readLine()) != null)
             System.out.println(str);
         sock.close();
