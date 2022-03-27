83a3ec9fea65c35f69d2e7c190121e24d6b4b85b
Adithya Pai B <paiadithya26@gmail.com>
2022-03-27 16:53:00 +0530

    UDP Server Size reduced

--- 4/udpServer.java
+++ 4/udpServer.java
@@ -1,11 +1,10 @@
 import java.util.*;
 import java.net.*;
-public class udpServer {
-    public static void main(String args [])throws Exception{
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter the Message");
-        String msg = s.nextLine();
-        new DatagramSocket().send(new DatagramPacket(msg.getBytes(),msg.length(),InetAddress.getLocalHost(),4000));
+class s {
+    public static void main(String args[]) throws Exception {
+        System.out.println("Enter a message : ");
+        String s = new Scanner(System.in).nextLine();
+        new DatagramSocket().send(new DatagramPacket(s.getBytes(), s.length(), InetAddress.getLocalHost(), 4000));
         System.out.println("Message Sent");
     }
 }
